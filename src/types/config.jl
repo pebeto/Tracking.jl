d69e9a4a9985794d172b1cda35f81fc6ae03109f
Jose Esparza <pebeto99@gmail.com>
2025-10-28 19:18:51 -0500

    Removing incorrect documentation in APIConfig type

--- src/types/config.jl
+++ src/types/config.jl
@@ -7,8 +7,7 @@ A struct to hold the configuration for the API server.
 - `host::String`: The host of the API server.
 - `port::Integer`: The port of the API server.
 - `db_file::String`: The path to the SQLite database file.
-- `jwt_secret::String`: The JWT secret for authentication. If not set, it defaults
-    to `Nothing`.
+- `jwt_secret::String`: The JWT secret for authentication.
 - `enable_auth::Bool`: Whether to enable authentication or not.
 - `enable_api::Bool`: Whether the API server is enabled or not.
 """
